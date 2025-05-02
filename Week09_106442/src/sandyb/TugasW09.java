30c56d875f73bd22ca0a7e6f8142d06aa8a91b31
[Sandy Bonfilio Yuvens] <[sandyby56@gmail.com]>
2025-05-02 15:44:51 +0700

    updated, not finished tho

--- Week09_106442/src/sandyb/TugasW09.java
+++ Week09_106442/src/sandyb/TugasW09.java
@@ -1,9 +1,80 @@
 package sandyb;
 
 public class TugasW09 {
+	public static class Device {
+		interface Connectivity {
+			boolean connectToWiFi(String networkName);
+
+			boolean disconnectFromWiFi();
+		}
+
+		String brand;
+		String model;
+
+		public void displayInfo() {
+			System.out.println("Brand\t: " + this.brand + "\nModel\t: " + this.model);
+		}
+	}
+
+	interface Power {
+		boolean turnOn();
+
+		boolean turnOff();
+	}
+
+	interface VolumeControl {
+		boolean increaseVolume(int level);
+
+		boolean decreaseVolume(int level);
+	}
+
+	public static class SmartPhone implements Power, VolumeControl, Device.Connectivity {
+		@Override
+		public boolean turnOn() {
+			return false;
+		}
+
+		@Override
+		public boolean turnOff() {
+			return false;
+		}
+
+		@Override
+		public boolean increaseVolume(int level) {
+			return false;
+		}
+
+		@Override
+		public boolean decreaseVolume(int level) {
+			return false;
+		}
+
+		@Override
+		public boolean connectToWiFi(String networkName) {
+			return false;
+		}
+
+		@Override
+		public boolean disconnectFromWiFi() {
+			return false;
+		}
+
+		int currentVolume;
+		String connectedNetwork;
+
+		public SmartPhone(int currentVolume, String connectedNetwork) {
+			super();
+			this.currentVolume = currentVolume;
+			this.connectedNetwork = connectedNetwork;
+		}
+	}
 
 	public static class Main {
 		public static void main(String[] args) {
+			SmartPhone smartPhone = new SmartPhone(1, "tes");
+//			SmartPhone.di
+
+			// how, kayaknya ada salah
 
 		}
 	}
